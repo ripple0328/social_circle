6f301b1f5142aa9c78f319ca2c66d78f24f58e3b
Qingbo Zhang <ripple0328@gmail.com>
2025-08-11 22:07:39 -0700

    fix build issue

--- test/social_circle_web/controllers/error_html_test.exs
+++ test/social_circle_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule SocialCircleWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(SocialCircleWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(SocialCircleWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
